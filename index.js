8ea4ff8576af192733fbc62a702b890e92a6b5f9
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-01-07 19:15:38 +0530

    wip re

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const db = require('./database')
 const fetch = require("node-fetch");
 const { google } = require("googleapis");
